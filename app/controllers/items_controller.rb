0ae365e376d027b061b9758b49644e96c79e47ba
TetoraAO <kob.617.dai@gmail.com>
2023-11-11 14:27:11 +0900

    商品出品機能の実装①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,23 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @items = Item.order("created_at DESC")
   end
-  
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_paramas
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :delivery_charge_id, :region_id, :delivery_date_id, :price).merge(user_id: current_user.id)
+  end
+
 end
