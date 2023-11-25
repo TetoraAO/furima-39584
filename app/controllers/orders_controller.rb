4fb184727c8ca9d7849ed056cd80bbbe0b0bee40
TetoraAO <kob.617.dai@gmail.com>
2023-11-25 13:40:24 +0900

    java記載、bundleインストール

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index, :show]
 
   def index
     if (@item.user_id == current_user.id) || @item.record.present?
@@ -11,6 +12,7 @@ class OrdersController < ApplicationController
   def create
     @purchase_record = PurchaseRecord.new(record_params)
     if @purchase_record.valid?
+      pay_item
       @purchase_record.save
       redirect_to root_path
     else
@@ -20,10 +22,19 @@ class OrdersController < ApplicationController
 
   private
   def record_params
-    params.require(:purchase_record).permit(:postal_code, :region_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id,:record_id).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_record).permit(:postal_code, :region_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id,:record_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: record_params[:token],
+        currency: 'jpy'
+      )
   end
 end
